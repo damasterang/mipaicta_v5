7f602cd01a34a70aad1c39dc2dbd855400e8d35e
damasterang <damasterang@outlook.com>
2014-07-06 21:20:00 +0700

    malam hujan

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+	$host = "localhost";
+	$username = "root";
+	$password = "abcd";
+	$database = "mindproreader";
+
+	mysql_connect($host,$username,$password);
+	mysql_select_db($database);
+
+?>
